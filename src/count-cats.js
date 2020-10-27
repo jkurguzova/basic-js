67c8929b3c08d0fcd39c5ea7d4c2233a4b8e1114
jkurguzova <jkurguzova@inbox.ru>
2020-10-27 21:32:34 +0300

    init: start basic js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(arr) {
+  let myArr = arr.reduce((acc, val) => acc.concat(val), []);
+    let catArr = myArr.filter(item => item == '^^');
+    let catsN = catArr.length;
+
+    return catsN;
 };
