bcfbf656f997e3e7f76b5ce6b6c7e44051f8cc9d
jkurguzova <jkurguzova@inbox.ru>
2020-10-28 21:23:42 +0300

    feat: solve hanoi and what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) {
+  return 'Unable to determine the time of year!';
+  } else if (Object.prototype.toString.call(date) !== '[object Date]') {
+  throw new Error();
+  } else {
+  let myMonth = date.getMonth();
+  if (myMonth > 1 && myMonth < 5) {
+  return 'spring';
+  } else if (myMonth > 4 && myMonth < 8) {
+  return 'summer';
+  } else if (myMonth > 7 && myMonth < 11) {
+  return 'fall';
+  } else {
+  return 'winter';
+  }
+
+  }
+
+  };
