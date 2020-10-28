bb3396e12ea47e3dee94b066f5e02ae89b5fd11f
jkurguzova <jkurguzova@inbox.ru>
2020-10-28 22:55:49 +0300

    feat: transform-array in process

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (Object.prototype.toString.call(arr) !== '[object Array]') {
+  throw new Error();
+  } else if (arr == [] || arr == Infinity) {
+  let myArr;
+  return myArr = [];
+  } else {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+  if (arr[i - 1] == '--discard-next') {
+  newArr.pop();
+  newArr[i] = "";
+  } else if (arr[i] == '--discard-prev') {
+  newArr.pop();
+  } else if (arr[i] == '--double-next') {
+  newArr.push(arr[i + 1]);
+  } else if (arr[i] == '--double-prev') {
+  newArr.push(arr[i - 1]);
+  } else {
+  newArr.push(arr[i]);
+  }
+  }
+  newArr = newArr.filter(function(el) {
+  return el !== undefined && el !=="" && el !== '--discard-next' && el !== '--discard-prev' && el !== '--double-next' && el !=='--double-prev';
+  });
+
+  return newArr;
+  }
+
+  };
